c94920f51245d719ddf53cdbe1f798ec8f088779
Gilles F <gilles@f.com>
2017-12-08 10:22:16 +0100

    GFE : add custom exception / tests corrections / refacto

---
+++ sg-bank-account-kata/src/main/java/com/sg/client/account/exception/AccountNotDebitedException.java
@@ -0,0 +1,8 @@
+package com.sg.client.account.exception;
+
+public class AccountNotDebitedException extends Exception {
+
+    public AccountNotDebitedException(String message) {
+        super(message);
+    }
+}
